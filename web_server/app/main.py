58ebb954098dd11b7ef43a590327f85ecef1fa84
Pavel <p.shahmin@gmail.com>
2024-04-20 22:35:44 +0300

    add: website html

--- web_server/app/main.py
+++ web_server/app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+from fastapi.responses import FileResponse
 
 app = FastAPI()
+templates = Jinja2Templates(directory="../templates")
+app.mount("/static", StaticFiles(directory="../static"), name="static")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
+
+
+@app.get("/process", response_class=HTMLResponse)
+async def process(request: Request):
+    return "Hello"
