43f1e97704d1812948cbc24a5b93d70bb7be4da9
Pavel <p.shahmin@gmail.com>
2024-04-20 22:53:44 +0300

    fix: paths

--- web_server/app/main.py
+++ web_server/app/main.py
@@ -3,11 +3,10 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
-from fastapi.responses import FileResponse
 
 app = FastAPI()
-templates = Jinja2Templates(directory="../templates")
-app.mount("/static", StaticFiles(directory="../static"), name="static")
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
